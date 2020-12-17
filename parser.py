07ddf154b98880fc3a786cf90413f4d9f23b2d73
Kimberly Elias <kim.eliasdiaz@gmail.com>
2020-12-17 10:56:59 -0600

    Correccción para análisis con clases

--- parser.py
+++ parser.py
@@ -3,6 +3,17 @@ from lex import *
 
 lexer = lex.lex()
 
+class Nodo():
+    def __init__(self, addr = '', code = ''):
+        self.addr = addr
+        self.code = code
+
+    def getAddr(self):
+        return self.addr
+
+    def getCode(self):
+        return self.code
+
 t = -1
 def getTemporal():
     global t 
@@ -17,40 +28,40 @@ precedence = (
 
 def p_add( p ) :
     'expr : expr PLUS expr'
-    p[0] = getTemporal()
-    print(p[0] + ' = ' + p[1] + ' + ' + p[3] + ';')
+    p[0] = Nodo(addr = getTemporal())
+    p[0].code = p[1].code + p[3].code + p[0].addr + ' = ' + p[1].addr + ' + ' + p[3].addr + ';\n'
 
 def p_sub( p ) :
     'expr : expr MINUS expr'
-    p[0] = getTemporal()
-    print(p[0] + ' = ' + p[1] + ' - ' + p[3] + ';')
+    p[0] = Nodo(addr = getTemporal())
+    p[0].code = p[1].code + p[3].code + p[0].addr + ' = ' + p[1].addr + ' - ' + p[3].addr + ';\n'
 
 def p_expr2uminus( p ) :
     'expr : MINUS expr %prec UMINUS'
-    p[0] = getTemporal()
-    print(p[0] + ' = -' + p[2] + ';')
+    p[0] = Nodo(addr = getTemporal())
+    p[0].code = p[2].code + p[0].addr + ' = -' + p[2].addr + ';\n'
 
 def p_mult_div( p ) :
     '''expr : expr TIMES expr
             | expr DIV expr'''
 
-    p[0] = getTemporal()
+    p[0] = Nodo(addr = getTemporal())
 
     if p[2] == '*' :
-       print(p[0] + ' = ' + p[1] + ' * ' + p[3] + ';')
+       p[0].code = p[1].code + p[3].code + p[0].addr + ' = ' + p[1].addr + ' * ' + p[3].addr + ';\n'
     else :
-        if p[3] == 0 :
+        if p[3].addr == 0 :
             print("Can't divide by 0")
             raise ZeroDivisionError('integer division by 0')
-        print(p[0] + ' = ' + p[1] + ' / ' + p[3] + ';')
+        p[0].code = p[1].code + p[3].code + p[0].addr + ' = ' + p[1].addr + ' / ' + p[3].addr + ';\n'
 
 def p_expr2NUM( p ) :
     'expr : NUMBER'
-    p[0] = str(p[1])
+    p[0] = Nodo(addr = str(p[1]))
 
 def p_parens( p ) :
     'expr : LPAREN expr RPAREN'
-    p[0] = str(p[2])
+    p[0] = Nodo(p[2].addr, p[2].code)
 
 def p_error( p ):
     print("Syntax error in input!")
@@ -59,3 +70,4 @@ import ply.yacc as yacc
 parser = yacc.yacc()
 
 res = parser.parse("5*9+10-(4/2*3)") # the input
+print(res.code)
